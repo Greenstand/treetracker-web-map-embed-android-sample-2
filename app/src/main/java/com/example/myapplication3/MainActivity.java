661b2694e868db15b07b07e917e9b968a05b9923
dadiorchen <dadirochen@outlook.com>
2024-04-23 16:14:43 +0800

    feat: can display webview, but no js seems

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication3;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,14 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
         // webview is the id of the WebView in the layout
         WebView webView = findViewById(R.id.webview);
-        webView.loadUrl("https://www.yahoo.com");
+        webView.loadUrl("https://leafletjs.com");
+        //Greeting("Android")
+//WebPage(url = "https://map.treetracker.org/wallets/9e143717-b34b-4e53-b810-6668d371adda?bounds=36.460189819335945,-0.23878028414651437,36.51889801025391,-0.16230561483658262&tree_id=1400272   ")
+        //WebPage(url = "https://map.treetracker.org")
+        // WebPage(url = "https://www.baidu.com")
+        // //WebPage(url = "https://dev.treetracker.org/top")
+        //WebPage(url ="https://leafletjs.com/examples/mobile/example.html")
+        //WebPage(url = "https://yahoo.com")
+        // WebPage(url = "https://leafletjs.com")
         // Set the padding to the WebView
         ViewCompat.setOnApplyWindowInsetsListener(webView, (v, insets) -> {
             Insets insets1 = insets.getInsets(WindowInsetsCompat.Type.systemBars());

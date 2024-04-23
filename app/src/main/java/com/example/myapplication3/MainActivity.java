c9d649b31e8fbc2155bf64fa9ae2c91e67cdee31
deanchen <antlet2004@gmail.com>
2024-04-23 15:30:27 +0800

    chore: try to add webview by ai

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -20,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // webview is the id of the WebView in the layout
+        WebView webView = findViewById(R.id.webview);
+        webView.loadUrl("https://www.yahoo.com");
+        // Set the padding to the WebView
+        ViewCompat.setOnApplyWindowInsetsListener(webView, (v, insets) -> {
+            Insets insets1 = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(insets1.left, insets1.top, insets1.right, insets1.bottom);
+            return insets;
+        });
     }
 }

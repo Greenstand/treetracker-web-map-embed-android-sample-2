b6fffe7d38a7a3e3c426209a6cc000cd78f1d5d4
dadiorchen <dadirochen@outlook.com>
2024-04-29 11:59:54 +0800

    chore: can not display wallet map, because the web map client issue

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -28,9 +28,10 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         //webView.loadUrl("https://leafletjs.com");
-        webView.loadUrl("https://leafletjs.com/examples/mobile/example.html");
+        //webView.loadUrl("https://leafletjs.com/examples/mobile/example.html");
         //webView.loadUrl("https://www.yahoo.com");
         //webView.loadUrl("https://admin.treetracker.org");
+        webView.loadUrl("https://map.treetracker.org/wallets/9e143717-b34b-4e53-b810-6668d371adda?bounds=36.460189819335945,-0.23878028414651437,36.51889801025391,-0.16230561483658262&tree_id=1400272");
         //Greeting("Android")
 //WebPage(url = "https://map.treetracker.org/wallets/9e143717-b34b-4e53-b810-6668d371adda?bounds=36.460189819335945,-0.23878028414651437,36.51889801025391,-0.16230561483658262&tree_id=1400272   ")
         //WebPage(url = "https://map.treetracker.org")
